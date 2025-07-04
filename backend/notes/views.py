5ba74458d210149781c973870447e467e55d50f9
abudi53 <abdallahffh@gmail.com>
2025-07-04 15:30:41 -0400

    backend - add viewsets and endpoints

--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Note
+from .serializers import NoteSerializer
 
-# Create your views here.
+
+class NoteViewSet(viewsets.ModelViewSet):
+    """
+    A simple ViewSet for viewing and editing notes.
+    """
+
+    queryset = Note.objects.all().order_by("-updated_at")
+    serializer_class = NoteSerializer
