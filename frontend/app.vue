4fc3384a6b5dc2400dc5c796f182cfb61b57a6e1
abudi53 <abdallahffh@gmail.com>
2025-07-05 22:39:10 -0400

    frontend - initial setup (shadcn - tailwind v4)

---
+++ frontend/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <Body class="bg-background text-foreground" />
+    <div class="container mx-auto p-4">
+      <h1 class="text-3xl font-bold mb-6">My Notes App</h1>
+      <Button>New Note</Button>
+      <NuxtPage />
+    </div>
+  </div>
+</template>
---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,34 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    'shadcn-nuxt'
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+})
